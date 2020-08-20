62b88f1a0cdd2f7be80bc578cca199619875b89e
progrm_jarvis <gh@progrm-jarvis.ru>
2020-08-21 01:50:24 +0300

    fix(classfile): refer to JVM Specification 14 in defs.rs

--- javac-rs-classfile/src/defs.rs
+++ javac-rs-classfile/src/defs.rs
@@ -4,30 +4,30 @@
 pub static CLASSFILE_HEADER: [u8; 4] = [0xCAu8, 0xFEu8, 0xBAu8, 0xBEu8];
 
 /// Name of a constructor
-/// [method](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.9)
+/// [method](https://docs.oracle.com/javase/specs/jvms/se14/html/jvms-2.html#jvms-2.9)
 pub static CONSTRUCTOR_METHOD_NAME: &str = "<init>";
 
 /// Name of a static initializer
-/// [method](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.9)
+/// [method](https://docs.oracle.com/javase/specs/jvms/se14/html/jvms-2.html#jvms-2.9)
 pub static STATIC_INITIALIZER_METHOD_NAME: &str = "<clinit>";
 
-/// `byte` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+/// `byte` [type](https://docs.oracle.com/javase/specs/jvms/se14/html/jvms-2.html#jvms-2.3.1)
 pub type JvmByte = i8;
 
-/// `short` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+/// `short` [type](https://docs.oracle.com/javase/specs/jvms/se14/html/jvms-2.html#jvms-2.3.1)
 pub type JvmShort = i16;
 
-/// `int` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+/// `int` [type](https://docs.oracle.com/javase/specs/jvms/se14/html/jvms-2.html#jvms-2.3.1)
 pub type JvmInt = i32;
 
-/// `long` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+/// `long` [type](https://docs.oracle.com/javase/specs/jvms/se14/html/jvms-2.html#jvms-2.3.1)
 pub type JvmLong = i64;
 
-/// `char` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+/// `char` [type](https://docs.oracle.com/javase/specs/jvms/se14/html/jvms-2.html#jvms-2.3.1)
 pub type JvmChar = u16;
 
-/// `float` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+/// `float` [type](https://docs.oracle.com/javase/specs/jvms/se14/html/jvms-2.html#jvms-2.3.1)
 pub type JvmFloat = f32;
 
-/// `double` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+/// `double` [type](https://docs.oracle.com/javase/specs/jvms/se14/html/jvms-2.html#jvms-2.3.1)
 pub type JvmDouble = f64;
