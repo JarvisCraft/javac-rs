6855f785243a365a8b5e84b8d954e72d860f1002
progrm_jarvis <gh@progrm-jarvis.ru>
2020-08-21 00:55:14 +0300

    chore(classfile): add defs.rs to store JVM-specific definitions

---
+++ javac-rs-classfile/src/defs.rs
@@ -0,0 +1,33 @@
+//! Generic classfile-specific definitions
+
+/// Header of Java class file
+pub static CLASSFILE_HEADER: [u8; 4] = [0xCAu8, 0xFEu8, 0xBAu8, 0xBEu8];
+
+/// Name of a constructor
+/// [method](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.9)
+pub static CONSTRUCTOR_METHOD_NAME: &str = "<init>";
+
+/// Name of a static initializer
+/// [method](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.9)
+pub static STATIC_INITIALIZER_METHOD_NAME: &str = "<clinit>";
+
+/// `byte` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+pub type JvmByte = i8;
+
+/// `short` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+pub type JvmShort = i16;
+
+/// `int` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+pub type JvmInt = i32;
+
+/// `long` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+pub type JvmLong = i64;
+
+/// `char` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+pub type JvmChar = u16;
+
+/// `float` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+pub type JvmFloat = f32;
+
+/// `double` [type](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-2.html#jvms-2.3.1)
+pub type JvmDouble = f64;
--- javac-rs-classfile/src/lib.rs
+++ javac-rs-classfile/src/lib.rs
@@ -1,1 +1,2 @@
 mod flag;
+mod defs;
