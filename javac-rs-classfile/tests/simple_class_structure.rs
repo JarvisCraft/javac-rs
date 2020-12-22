560b3229141d5970506c2213203f9d14b471d043
Petr Portnov <gh@progrm-jarvis.ru>
2020-12-22 23:11:40 +0300

    test(classfile): test addition of multiple custom attributes

--- javac-rs-classfile/tests/simple_class_structure.rs
+++ javac-rs-classfile/tests/simple_class_structure.rs
@@ -53,7 +53,7 @@ fn create_minimal_class_file_with_multiple_interface() {
 }
 
 #[test]
-fn create_minimal_class_file_with_custom_attribute() {
+fn create_minimal_class_file_with_single_custom_attribute() {
     let mut class = Class::new(
         ClassfileVersion::of_major(major_versions::JAVA_14),
         ClassAccessFlag::Public | ClassAccessFlag::Final | ClassAccessFlag::Super,
@@ -67,6 +67,30 @@ fn create_minimal_class_file_with_custom_attribute() {
     let class = class;
 
     let mut file = File::create("TestClassWithSingleCustomAttribute.class").unwrap();
+    println!("{:#?}", class);
+    class.write_to_classfile(&mut file);
+    println!("Written to file: {:#?}", file);
+}
+
+#[test]
+fn create_minimal_class_file_with_multiple_custom_attributes() {
+    let mut class = Class::new(
+        ClassfileVersion::of_major(major_versions::JAVA_14),
+        ClassAccessFlag::Public | ClassAccessFlag::Final | ClassAccessFlag::Super,
+        String::from("ru/progrm_jarvis/javacrs/TestClassWithMultipleCustomAttributes"),
+        String::from("java/lang/Object"),
+    );
+    class.add_custom_attribute(
+        String::from("SomeCustomAttribute"),
+        JvmVecU4::try_from(Vec::from("Hello world".as_bytes())).unwrap(),
+    ).unwrap();
+    class.add_custom_attribute(
+        String::from("OtherCustomAttribute"),
+        JvmVecU4::try_from(Vec::from("How r u?".as_bytes())).unwrap(),
+    ).unwrap();
+    let class = class;
+
+    let mut file = File::create("TestClassWithMultipleCustomAttributes.class").unwrap();
     println!("{:#?}", class);
     class.write_to_classfile(&mut file);
     println!("Written to file: {:#?}", file);
